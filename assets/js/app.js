932f225d251a9f3e3f5bc10f4b739bca975dff14
Simon Escure <simon_escure@yahoo.fr>
2018-10-25 16:33:17 +0200

    SCSS sidenav

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
   
-  const hamburger = document.getElementById('hamburger-menu');
-  const lines = document.querySelectorAll('.line');
+  const hamburger        = document.getElementById('hamburger-menu');
+  const lines            = document.querySelectorAll('.line');
+  const sidenavContainer = document.querySelector('.sidenav-container');
+  const submenu          = document.querySelector('.submenu');
+
+  console.log(submenu);
+  
+
   
   hamburger.addEventListener('click', () => {
     hamburger.classList.contains('is-clicked') ? hamburger.classList.remove('is-clicked') : hamburger.classList.add('is-clicked');
+    sidenavContainer.classList.toggle('hidden');
   })
 
 

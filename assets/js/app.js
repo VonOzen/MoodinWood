0b595d9c624773a8bfe2880b3cc0cc9d9b7bb8a9
Simon Escure <simon_escure@yahoo.fr>
2018-10-25 15:31:10 +0200

    adding some JS, SCSS

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+  
+  const hamburger = document.getElementById('hamburger-menu');
+  const lines = document.querySelectorAll('.line');
+  
+  hamburger.addEventListener('click', () => {
+    hamburger.classList.contains('is-clicked') ? hamburger.classList.remove('is-clicked') : hamburger.classList.add('is-clicked');
+  })
 
+
+});

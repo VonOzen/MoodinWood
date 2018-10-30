c734758609fd312f8d3080b0e0dad79432bee414
Simon Escure <simon_escure@yahoo.fr>
2018-10-30 15:09:40 +0100

    Rethinking layout and responsive menu SCSS

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
   
-  const hamburger        = document.getElementById('hamburger-menu');
-  const lines            = document.querySelectorAll('.line');
-  const sidenavContainer = document.querySelector('.sidenav-container');
-  const submenu          = document.querySelector('.submenu');
-
-  console.log(submenu);
-  
+  const hamburger = document.getElementById('hamburger-menu');
+  const lines     = document.querySelectorAll('.line');
+  const body      = document.querySelector('body');
 
   
-  hamburger.addEventListener('click', () => {
+  hamburger.addEventListener('click', (e) => {
+    e.preventDefault();
+    body.classList.toggle('sidebar-open');
     hamburger.classList.contains('is-clicked') ? hamburger.classList.remove('is-clicked') : hamburger.classList.add('is-clicked');
-    sidenavContainer.classList.toggle('hidden');
   })
-
+  
+  document.getElementById('backdrop').addEventListener('click', function(){
+    body.classList.remove('sidebar-open');
+    hamburger.classList.remove('is-clicked');
+  });
 
 });

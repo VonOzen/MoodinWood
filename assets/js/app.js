08291eeb6c1ce3456935496e0238b91a8cd82de5
Simon Escure <simon_escure@yahoo.fr>
2018-11-08 11:49:46 +0100

    Updating SCSS - responsive - posts index page and post partial

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,18 +2,17 @@ document.addEventListener("DOMContentLoaded", function() {
   
   const hamburger = document.getElementById('hamburger-menu');
   const lines     = document.querySelectorAll('.line');
-  const body      = document.querySelector('body');
+  const menu      = document.querySelector('.menu');
 
   
   hamburger.addEventListener('click', (e) => {
     e.preventDefault();
-    body.classList.toggle('sidebar-open');
     hamburger.classList.contains('is-clicked') ? hamburger.classList.remove('is-clicked') : hamburger.classList.add('is-clicked');
+    menu.classList.toggle('is-open');
   })
   
-  document.getElementById('backdrop').addEventListener('click', function(){
-    body.classList.remove('sidebar-open');
-    hamburger.classList.remove('is-clicked');
+  document.querySelector('main').addEventListener('click', function(){
+    menu.classList.remove('is-open');
   });
 
 });

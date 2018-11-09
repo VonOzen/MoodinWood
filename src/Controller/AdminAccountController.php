86939f45c63d25b5fa3fbe22f67324fc7a05d4cd
Simon Escure <simon_escure@yahoo.fr>
2018-11-09 11:23:22 +0100

    Adding User entity - Admin login / logout - Admin login template - SCSS

---
+++ src/Controller/AdminAccountController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class AdminAccountController extends AbstractController
+{
+    /**
+     * @Route("/admin/login", name="admin_login")
+     * 
+     * @param AuthenticationUtils $utils
+     * @return Response
+     */
+    public function login(AuthenticationUtils $utils)
+    {
+        $error = $utils->getLastAuthenticationError();
+        return $this->render('admin/account/login.html.twig', [
+            'hasError' => $error !== null, 
+            'username' => $utils->getLastUsername()
+        ]);
+    }
+
+    /**
+     * Allow admin logout
+     *
+     * @Route("admin/logout", name="admin_logout")
+     * 
+     * @return void
+     */
+    public function logout()
+    {
+        // ...
+    }
+}
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,17 +4,35 @@ namespace App\DataFixtures;
 
 use Faker\Factory;
 use App\Entity\Post;
+use App\Entity\User;
 use Cocur\Slugify\Slugify;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+  private $encoder;
+
+  public function __construct(UserPasswordEncoderInterface $encoder)
+  {
+    $this->encoder = $encoder;
+  }
+
   public function load(ObjectManager $manager)
   {
     $faker   = Factory::create('fr_FR');
     $slugify = new Slugify();
 
+    // Adding fake user
+    $admin = new User();
+    $admin->setUsername('Admin')
+          ->setEmail('hello@simonescure.fr')
+          ->setHash($this->encoder->encodePassword($admin, 'admin'));
+    
+    $manager->persist($admin);
+
+    // Adding fake posts
     for($i = 1; $i <= 20; $i++){
       $post = new Post();
 
